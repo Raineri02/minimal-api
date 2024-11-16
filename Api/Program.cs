aa290f00c2d0bb9ce8ea646fbdc6582131691679
Raineri <asa.raineri20@gmail.com>
2024-11-15 23:35:30 -0300

    Feito

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,23 @@
-using MinimalApi;
+using MinimalApi.DTOs;
 
-IHostBuilder CreateHostBuilder(string[] args){
-  return Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.UseStartup<Startup>();
-    });
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", ()=> "Ola pessoal");
+
+app.MapPost("/login", (LoginDTO LoginDTO) => {
+    if(LoginDTO.Email == "adm@teste.com" && LoginDTO.Senha == "123456"){
+         return Results.Ok ("Login com sucesso");
+    }
+    else
+         return Results.Unauthorized();
+
+});
+
+
+public class LoginDTO
+{
+  public string Email {get; set;} = default!;
+  public string Senha {get; set;} = default !;
+  
 }
-
-CreateHostBuilder(args).Build().Run();
